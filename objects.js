3b860e124187425cd1a401208cc1ded29092b99b
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-17 07:05:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,3 +5,8 @@ var playlist = {
 function updatePlaylist(playlist, name, title){
   return Object.assign({}, playlist, {[name]: title})
 }
+
+function removeFromPlaylist(playlist, name){
+  delete playlist.name
+  return playlist
+}
