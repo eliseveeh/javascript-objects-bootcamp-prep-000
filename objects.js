2d415b044e1353969f1c0a8cde6819e071843a48
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-17 07:00:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
 var playlist = {
-  name: title;
-}
-function updatePlaylist(playlist, name, title){
-  Object.assign ({}, playlist, {[name]: title})
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, name){
-  delete playlist.name;
+  wean: "Ocean man"
 }
